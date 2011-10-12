96b8b83841e143659fe53a0d173f8aa88c6e2929
miguel <miguel.perez@koombea.com>
2011-10-12 15:27:29 -0500

    updating recipes with the engineyard ones

--- cookbooks/config_files/recipes/default.rb
+++ cookbooks/config_files/recipes/default.rb
@@ -3,14 +3,15 @@
 # Recipe:: default
 #
 app_name = "rfspotcube"
-
-template "/data/#{app_name}/shared/config/omniauth.yml" do
-  owner node[:owner_name]
-  group node[:owner_name]
-  mode 0644
-  source "omniauth.yml.erb"
-  variables({
-    :facebook => {:token => "167590113286949", :secret => "d744ae91dbb13bde96224d145ffa7bf8"},
-    :twitter => {:token => "qt0dxH2jEWr6bH8WLSZBQ", :secret => "QRTz6z7O7NUx3ZCKoAztYMigv7KtUrC3188cWiDkc"},
-  })
+if ['app_master', 'app', 'solo'].include?(node[:instance_role]) then
+  template "/data/#{app_name}/shared/config/omniauth.yml" do
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    source "omniauth.yml.erb"
+    variables({
+      :facebook => {:token => "167590113286949", :secret => "d744ae91dbb13bde96224d145ffa7bf8"},
+      :twitter => {:token => "qt0dxH2jEWr6bH8WLSZBQ", :secret => "QRTz6z7O7NUx3ZCKoAztYMigv7KtUrC3188cWiDkc"},
+    })
+  end
 end
