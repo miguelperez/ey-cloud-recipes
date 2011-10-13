d0c0ec39b54580bb887b4d8d4c4ef624ec383e46
miguel <miguel.perez@koombea.com>
2011-10-13 17:47:42 -0500

    adding recipe for creating a template

--- cookbooks/postgis/recipes/default.rb
+++ cookbooks/postgis/recipes/default.rb
@@ -2,4 +2,5 @@ if ['solo', 'db_master'].include?(node[:instance_role])
   require_recipe "postgis::geos"
   require_recipe "postgis::proj"
   require_recipe "postgis::postgis"
+  require_recipe "postgis::template"
 end
