bd2d8c4f791dca323e691acf4e5524c533322814
miguel <miguel.perez@koombea.com>
2011-10-14 16:18:14 -0500

    sudo the emerge postgres config action

--- cookbooks/postgis/recipes/restart_database.rb
+++ cookbooks/postgis/recipes/restart_database.rb
@@ -1,7 +1,7 @@
 bash "restart_database" do
   user "deploy"
   code <<-EOH
-    emerge postgresql-server --config
+    sudo emerge postgresql-server --config
     
     sudo /etc/init.d/postgresql-8.3 restart
   EOH
